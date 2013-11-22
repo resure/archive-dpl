9581ea9c3824ed87fdc064e01c191cb53f944c0c
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2013-11-22 15:35:21 +0400

    Main frontend functions

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('frontendApp')
   .controller('MainCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
       var ref = new Firebase('https://dpl-dev.firebaseio.com/');
       angularFire(ref.child('versions'), $scope, 'fireVersions');
-      angularFire(ref.child('status'), $scope, 'status');
+      angularFire(ref.child('status'), $scope, 'fireStatus');
       angularFire(ref.child('current'), $scope, 'current');
 
       $scope.$watch('fireVersions', function (versions) {
@@ -12,5 +12,17 @@ angular.module('frontendApp')
           angular.forEach(versions, function (version) {
               $scope.versions.push(JSON.parse(version.payload));
           });
+      });
+
+      $scope.deploy = function (commit) {
+          ref.child('status').set(JSON.stringify({
+              action: 'deploy',
+              commit: commit
+          }));
+      };
+
+      $scope.$watch('fireStatus', function (status) {
+          if (status !== undefined)
+              $scope.status = JSON.parse(status);
       });
   }]);
